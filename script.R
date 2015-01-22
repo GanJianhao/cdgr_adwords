ccd616dfe8837ba3e3388c21859e3aa35b8286d9
Thomas <thomas.antonakis@gmail.com>
2015-01-22 19:01:53 +0200

    Plotting some graphs

--- script.R
+++ script.R
@@ -23,9 +23,10 @@ rm(accs)
 
 # Start a report data.frame
 report<-data.frame(year = 0,
-                   week = 0, 
+                   week = 0,
+                   date = 0,
                    adCostB = 0, 
-                   adcostNB = 0,
+                   adCostNB = 0,
                    regB = 0,
                    regNB = 0, 
                    clickB = 0, 
@@ -41,7 +42,7 @@ enddate = startdate+6
 weeksleft<-as.numeric(today-startdate) %/% 7
 
 # Potential loop start
-# while (weeksleft!=0) {
+while (weeksleft!=0) {
 # Fetch
 
 adwords<-get_ga(25764841, start.date = startdate, end.date = enddate,
@@ -101,55 +102,74 @@ search$cpo<-search$adCost / search$goal1Completions
 
 #Start filling the report dataframe with weekly data
 tbadded<-data.frame(year = 0,
-                    week = 0, 
+                    week = 0,
+                    date = 0,
                     adCostB = 0, 
-                    adcostNB = 0,
+                    adCostNB = 0,
                     regB = 0,
                     regNB = 0, 
                     clickB = 0, 
                     clickNB = 0                   
 )
+
 tbadded$year <- year(enddate)
 tbadded$week <-  week(enddate)
+tbadded$date <- enddate
 tbadded$adCostB <- search$adCost[search$campaign == 'Brand']
-tbadded$adcostNB <- sum(search$adCost[search$campaign != 'Brand'])
+tbadded$adCostNB <- sum(search$adCost[search$campaign != 'Brand'])
 tbadded$regB <- search$goal6Completions[search$campaign == 'Brand']
 tbadded$regNB <- sum(search$goal6Completions[search$campaign != 'Brand'])
 tbadded$clickB <- search$adClicks[search$campaign == 'Brand']
 tbadded$clickNB <- sum(search$adClicks[search$campaign != 'Brand'])
 
+startdate=startdate+7
+enddate=startdate+6
 
-# startdate=startdate+7
-# enddate=startdate+6
-# 
-# weeksleft<-as.numeric(today-startdate) %/% 7
-# # kati se rbind       
+weeksleft<-as.numeric(today-startdate) %/% 7
+# kati se rbind       
+report<-rbind(report,tbadded )       
+} # Potential Loop end
         
-# } Potential Loop end
-        
-        
+report<-report[-1,]  
+report$cprB<-report$adCostB / report$regB
+report$cprNB<-report$adCostNB / report$regNB
+
+## Plotting
+
+# Ad Costs All campaigns
+plot(report$date, report$adCostNB, type= 'h', lwd = 4, col = 'deepskyblue1', ylim = c(0,10000),
+     main = "Adwords Costs per week (Brand excl.)" ,xlab="Date", ylab = "Ad Cost")
+
+# Ad Costs Brand campaign
+plot(report$date, report$adCostB, type= 'h', lwd = 4, col = 'deepskyblue2', 
+     main = "Brand Campaign Costs per week" ,xlab="Date", ylab = "Ad Cost")
+
+# Registrations
+library(ggplot2)
+
+
 # rm(adwords)
 
 
 
 # Request for Android App new Installs
-android<-get_ga(81060646, start.date = startdate, end.date = enddate,
-                 
-                 metrics = "
-                        ga:goal1Completions, 
-                        ga:newUsers
-                ",
-                 
-                 dimensions = "ga:medium",
-                 sort ="-ga:newUsers" ,
-                 filters =  "ga:medium == cpc",
-                 segment = NULL, 
-                 sampling.level = NULL,
-                 start.index = NULL, 
-                 max.results = NULL, 
-                 ga_token,
-                 verbose = getOption("rga.verbose")
-)
+# android<-get_ga(81060646, start.date = startdate, end.date = enddate,
+#                  
+#                  metrics = "
+#                         ga:goal1Completions, 
+#                         ga:newUsers
+#                 ",
+#                  
+#                  dimensions = "ga:medium",
+#                  sort ="-ga:newUsers" ,
+#                  filters =  "ga:medium == cpc",
+#                  segment = NULL, 
+#                  sampling.level = NULL,
+#                  start.index = NULL, 
+#                  max.results = NULL, 
+#                  ga_token,
+#                  verbose = getOption("rga.verbose")
+# )
 
 # Check how all data can be included in one data.frame
 # check how we can retrieve data from previous weks for comparison
@@ -160,4 +180,4 @@ android<-get_ga(81060646, start.date = startdate, end.date = enddate,
 # write.xlsx()
 
 # Environment Size 
-print(paste("Size:", format(object.size(ls()), unit="Kb")))
+# print(paste("Size:", format(object.size(ls()), unit="Kb")))
