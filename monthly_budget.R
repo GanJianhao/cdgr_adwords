24782eddf312800e6f76b594c8fddaf467de712c
Thomas <thomas.antonakis@gmail.com>
2015-02-06 16:29:09 +0200

    SQL data extract and manipulation

---
+++ monthly_budget.R
@@ -0,0 +1,393 @@
+####################################################
+##########  Monthly ## Adwords Report ##############
+####################################################
+ptm <- proc.time()
+
+setwd("C:/Users/tantonakis/Google Drive/Scripts/AnalyticsProj/cdgr_adwords/Budget")
+
+library(RGA)
+library(RMySQL)
+library(xlsx)
+library(tidyr)
+library(plyr)
+library(dplyr)
+
+client.id = '543269518849-dcdk7eio32jm2i4hf241mpbdepmifj00.apps.googleusercontent.com'
+client.secret = '9wSw6gyDVXtcgqEe0XazoBWG'
+
+ga_token<-authorize(client.id, client.secret, cache = getOption("rga.cache"),
+                    verbose = getOption("rga.verbose"))
+
+accs<-list_profiles(account.id = "~all", webproperty.id = "~all",
+                    start.index = NULL, max.results = NULL, ga_token,
+                    verbose = getOption("rga.verbose"))
+
+accounts<-data.frame(id = accs$id)
+accounts$desc<-c('website', 'android', 'ios', 'youtube')
+rm(accs)
+
+
+####################################################
+############### SOS!! CHANGE DATE###################
+####################################################
+startdate = as.Date('2014-01-01')
+enddate = as.Date('2014-12-31')
+
+
+adwords_init<-get_ga(25764841, start.date = startdate, end.date = enddate,
+                
+                metrics = "
+                        ga:impressions,
+                        ga:adClicks,
+                        ga:sessions,
+                        ga:newUsers,
+                        ga:adCost,
+                        ga:goal6Completions
+                ",
+                
+                dimensions = "
+                        ga:campaign,
+                        ga:yearMonth
+                ",
+                sort = "
+                        ga:yearMonth,
+                        -ga:impressions
+                ", 
+                filters = NULL,
+                segment = NULL, 
+                sampling.level = NULL,
+                start.index = NULL, 
+                max.results = NULL, 
+                ga_token,
+                verbose = getOption("rga.verbose")
+)
+# adwords
+
+# Get rid of zero impression lines
+adwords_init <- adwords_init[adwords_init$impressions !=0,]
+
+
+impressions<-select(adwords_init, campaign, yearMonth, impressions) %>% spread(yearMonth, impressions)
+impressions$metric<-'impressions'
+
+adClicks<-select(adwords_init, campaign, yearMonth, adClicks) %>% spread(yearMonth, adClicks)
+adClicks$metric<-'adClicks'
+
+sessions<-select(adwords_init, campaign, yearMonth, sessions) %>% spread(yearMonth, sessions)
+sessions$metric<-'sessions'
+
+newUsers<-select(adwords_init, campaign, yearMonth, newUsers) %>% spread(yearMonth, newUsers)
+newUsers$metric<-'newUsers'
+
+adCost<-select(adwords_init, campaign, yearMonth, adCost) %>% spread(yearMonth, adCost)
+adCost$metric<-'adCost'
+
+reg<-select(adwords_init, campaign, yearMonth, goal6Completions) %>% spread(yearMonth, goal6Completions)
+reg$metric<-'registrations'
+
+report<- rbind(impressions, adClicks, sessions, newUsers,adCost, reg  )
+report[is.na(report)]<-0
+
+report$cat<-0
+report$cat[grep("Remarketing", report$campaign , ignore.case=FALSE, fixed=FALSE)]<-"Remarketing"
+report$cat[grep("DSP", report$campaign , ignore.case=TRUE, fixed=FALSE)]<-"Display"
+report$cat[grep("App. Android", report$campaign , ignore.case=FALSE, fixed=FALSE)]<-"Android"
+report$cat[grep("App. iOS", report$campaign , ignore.case=FALSE, fixed=FALSE)]<-"iOS"
+report$cat[grep("Brand", report$campaign , ignore.case=FALSE, fixed=FALSE)]<-"Brand"
+
+
+report$cat[grep("Mundobasket", report$campaign , ignore.case=TRUE, fixed=FALSE)]<-"Youtube"
+report$cat[grep("TV #match_day_offer", report$campaign , ignore.case=FALSE, fixed=FALSE)]<-"Youtube"
+report$cat[grep("TV generic", report$campaign , ignore.case=FALSE, fixed=FALSE)]<-"Youtube"
+report$cat[report$cat==0]<-"Rest"
+report<-report[,c(1,14,15,2:13)]
+rm(adwords_init)
+
+
+free_seg<-get_ga(25764841, start.date = startdate, end.date = enddate,
+                     
+                     metrics = "
+                        ga:impressions,
+                        ga:adClicks,
+                        ga:sessions,
+                        ga:newUsers,
+                        ga:adCost,
+                        ga:goal6Completions
+                ",
+                     
+                     dimensions = "
+                        ga:channelGrouping,
+                        ga:yearMonth
+                ",
+                     sort = "
+                        ga:yearMonth,
+                        -ga:impressions
+                ", 
+                     filters = NULL,
+                     segment = NULL, 
+                     sampling.level = NULL,
+                     start.index = NULL, 
+                     max.results = NULL, 
+                     ga_token,
+                     verbose = getOption("rga.verbose")
+)
+# free_seg
+free_seg<-select(free_seg,channelGrouping, yearMonth, sessions, newUsers, goal6Completions)
+
+sessions<-select(free_seg,channelGrouping, yearMonth, sessions) %>% spread(yearMonth, sessions)
+sessions$metric<-'sessions'
+
+newUsers<-select(free_seg,channelGrouping, yearMonth, newUsers) %>% spread(yearMonth, newUsers)
+newUsers$metric<-'newUsers'
+
+registrations<-select(free_seg,channelGrouping, yearMonth, goal6Completions) %>% spread(yearMonth, goal6Completions)
+registrations$metric<-'registrations'
+
+web_free_seg<- rbind(sessions, newUsers, registrations)
+web_free_seg[is.na(web_free_seg)]<-0
+
+rm(accounts, adClicks, adCost,  free_seg, impressions, newUsers, reg, registrations, sessions)
+
+
+and_free<-get_ga(81060646, start.date = startdate, end.date = enddate,
+                     
+                     metrics = "
+                        ga:sessions,
+                        ga:newUsers
+                ",
+                     
+                     dimensions = "
+                        ga:yearMonth
+                ",
+                     sort = "
+                        ga:yearMonth                        
+                ", 
+                     filters = NULL,
+                     segment = NULL, 
+                     sampling.level = NULL,
+                     start.index = NULL, 
+                     max.results = NULL, 
+                     ga_token,
+                     verbose = getOption("rga.verbose")
+)
+and_free$app<-'android'
+
+
+ios_free<-get_ga(81074931, start.date = startdate, end.date = enddate,
+                 
+                 metrics = "
+                        ga:sessions,
+                        ga:newUsers
+                ",
+                 
+                 dimensions = "
+                        ga:yearMonth
+                ",
+                 sort = "
+                        ga:yearMonth                        
+                ", 
+                 filters = NULL,
+                 segment = NULL, 
+                 sampling.level = NULL,
+                 start.index = NULL, 
+                 max.results = NULL, 
+                 ga_token,
+                 verbose = getOption("rga.verbose")
+)
+ios_free$app<-'ios'
+
+mob_free<-rbind(and_free, ios_free)
+rm(and_free, ios_free)
+
+sessions<-select(mob_free, app, yearMonth, sessions) %>% spread(yearMonth, sessions)
+sessions$cat<-'sessions'
+newUsers<-select(mob_free, app, yearMonth, newUsers) %>% spread(yearMonth, newUsers)
+newUsers$cat<-'newUsers'
+mob_free<-rbind(sessions, newUsers)
+mob_free<-mob_free[,c(1,14,2:13)]
+rm(newUsers, sessions)
+
+gc()
+proc.time() - ptm
+write.xlsx(x = report, file = "adwords.xlsx", row.names = FALSE)
+write.xlsx(x = web_free_seg, file = "web_free.xlsx", row.names = FALSE)
+write.xlsx(x = mob_free, file = "mob_free.xlsx", row.names = FALSE)
+proc.time() - ptm
+# Environment Size 
+# print(paste("Size:", format(object.size(ls()), unit="Kb")))
+
+###########################################################
+################# mySQL calculations ######################
+###########################################################
+
+# Open VPN !!!
+
+###################
+# Verifications
+###################
+# Establish connection
+con <- dbConnect(RMySQL::MySQL(), host = 'db.clickdelivery.gr', port = 3307, dbname = "beta",
+                 user = "tantonakis", password = "2secret4usAll!")
+# Send query
+rs <- dbSendQuery(con,"
+
+SELECT            MONTH(FROM_UNIXTIME(`user_master`.`i_date`)) as MONTH,
+                  `user_master`.`referal_source` AS SOURCE ,
+                  COUNT(*) AS VERIFIED_USERS
+                  FROM `user_master`
+                  WHERE `user_master`.`verification_date` >= UNIX_TIMESTAMP('2014-01-01')
+                  AND `user_master`.`verification_date` < UNIX_TIMESTAMP('2015-01-01')
+                  AND `user_master`.`status` = 'VERIFIED'
+                  AND `user_master`.`is_deleted` = 'N'
+                  GROUP BY `user_master`.`referal_source`, MONTH(FROM_UNIXTIME(`user_master`.`i_date`))
+                  
+                  ")
+# Fetch query results (n=-1) means all results
+verified_src <- dbFetch(rs, n=-1) 
+
+# close connection
+dbDisconnect(con)
+# Stop timer
+proc.time() - ptm
+
+
+############################################################################
+# Registrations
+############################################################################
+# Establish connection
+con <- dbConnect(RMySQL::MySQL(), host = 'db.clickdelivery.gr', port = 3307, dbname = "beta",
+                 user = "tantonakis", password = "2secret4usAll!")
+# Send query
+rs <- dbSendQuery(con,"
+                  
+SELECT MONTH(FROM_UNIXTIME(`user_master`.`i_date`)) as MONTH,
+                `user_master`.`referal_source` AS SOURCE,
+                COUNT(*) AS REGISTERED_USERS
+                FROM `user_master`
+		WHERE `user_master`.`i_date` >= UNIX_TIMESTAMP('2014-01-01')
+		AND `user_master`.`i_date` < UNIX_TIMESTAMP('2015-01-01')
+		AND `user_master`.`is_deleted` = 'N'
+		GROUP BY `user_master`.`referal_source`, MONTH(FROM_UNIXTIME(`user_master`.`i_date`))
+                  
+                  
+                  ")
+# Fetch query results (n=-1) means all results
+registered_src <- dbFetch(rs, n=-1) 
+
+# close connection
+dbDisconnect(con)
+# Stop timer
+proc.time() - ptm
+# Clean UP
+gc()
+
+#########################################
+# Orders From mySQL
+#########################################
+
+# Establish connection
+con <- dbConnect(RMySQL::MySQL(), host = 'db.clickdelivery.gr', port = 3307, dbname = "beta",
+                 user = "tantonakis", password = "2secret4usAll!")
+# Send query
+rs <- dbSendQuery(con,"
+                  
+                  SELECT MONTH(FROM_UNIXTIME(`order_master`.`i_date`)) as MONTH,
+                       `order_master`.`order_referal` AS SOURCE ,
+                         COUNT(*) AS VERIFIED_ORDERS
+                  FROM `order_master`
+
+                  WHERE `order_master`.`i_date` >= UNIX_TIMESTAMP('2014-01-01')
+                  AND `order_master`.`i_date` < UNIX_TIMESTAMP('2015-01-01')
+                  AND `order_master`.`status` IN ('VERIFIED', 'REJECTED')
+                  AND `order_master`.`is_deleted` = 'N'
+                  GROUP BY  `order_master`.`order_referal`, MONTH(FROM_UNIXTIME(`order_master`.`i_date`))
+                  
+                  ")
+# Fetch query results (n=-1) means all results
+orders_src <- dbFetch(rs, n=-1) 
+# close connection
+dbDisconnect(con)
+# Stop timer
+proc.time() - ptm
+# Clean UP
+gc()
+ 
+registered_src$cat<-""
+verified_src$cat<-""
+orders_src$cat<-""
+
+# Android
+registered_src$cat[registered_src$SOURCE == "Android"]<-"android"
+verified_src$cat[verified_src$SOURCE == "Android"]<-"android"
+orders_src$cat[orders_src$SOURCE == "Android"]<-"android"
+# iOS
+registered_src$cat[registered_src$SOURCE == "IOS"]<-"ios"
+verified_src$cat[verified_src$SOURCE == "IOS"]<-"ios"
+orders_src$cat[orders_src$SOURCE == "IOS"]<-"ios"
+# Organic
+## Google
+registered_src$cat[grep("google", registered_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+verified_src$cat[grep("google", verified_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+orders_src$cat[grep("google", orders_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+## Yahoo
+registered_src$cat[grep("yahoo", registered_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+verified_src$cat[grep("yahoo", verified_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+orders_src$cat[grep("yahoo", orders_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+## Bing
+registered_src$cat[grep("bing", registered_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+verified_src$cat[grep("bing", verified_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+orders_src$cat[grep("bing", orders_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+## Search
+registered_src$cat[grep("search", registered_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+verified_src$cat[grep("search", verified_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+orders_src$cat[grep("search", orders_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"organic"
+
+# Newsletter
+registered_src$cat[grep("newsletter", registered_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"newsletter"
+verified_src$cat[grep("newsletter", verified_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"newsletter"
+orders_src$cat[grep("newsletter", orders_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"newsletter"
+
+# Facebook
+registered_src$cat[grep("facebook", registered_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"facebook"
+verified_src$cat[grep("facebook", verified_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"facebook"
+orders_src$cat[grep("facebook", orders_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"facebook"
+
+# Affiliate
+registered_src$cat[grep("linkwise", registered_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"affiliate"
+verified_src$cat[grep("linkwise", verified_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"affiliate"
+orders_src$cat[grep("linkwise", orders_src$SOURCE , ignore.case=FALSE, fixed=FALSE)]<-"affiliate"
+
+# Direct 
+registered_src$cat[registered_src$cat == ""]<-"direct"
+verified_src$cat[verified_src$cat == ""]<-"direct"
+orders_src$cat[orders_src$cat == ""]<-"direct"
+
+# Adwords
+registered_src$cat[grep("google|cpc", registered_src$SOURCE , ignore.case=FALSE, fixed=TRUE)]<-"adwords"
+verified_src$cat[grep("google|cpc", verified_src$SOURCE , ignore.case=FALSE, fixed=TRUE)]<-"adwords"
+orders_src$cat[grep("google|cpc", orders_src$SOURCE , ignore.case=FALSE, fixed=TRUE)]<-"adwords"
+
+registered_src<-ddply(registered_src,c("cat", "MONTH"), summarize, registration=sum(REGISTERED_USERS))
+verified_src<-ddply(verified_src,c("cat", "MONTH"), summarize, verifications=sum(VERIFIED_USERS))
+orders_src<-ddply(orders_src,c("cat", "MONTH"), summarize, orders=sum(VERIFIED_ORDERS))
+
+registered_src<-registered_src %>% spread(MONTH, registration)
+registered_src$metric<-'registrations'
+verified_src<-verified_src %>% spread(MONTH, verifications)
+verified_src$metric<-'verifications'
+orders_src<-orders_src %>% spread(MONTH, orders)
+orders_src$metric<-'orders'
+registered_src[is.na(registered_src)]<-0
+verified_src[is.na(verified_src)]<-0
+orders_src[is.na(orders_src)]<-0
+
+# Export final dataframes
+# write.xlsx(x = registration_src, file = "registration.xlsx",
+#            sheetName = "Sheet 1", row.names = FALSE)
+# write.xlsx(x = verified_src, file = "verification.xlsx",
+#            sheetName = "Sheet 1", row.names = FALSE)
+# write.xlsx(x = orders_src, file = "order.xlsx",
+#            sheetName = "Sheet 1", row.names = FALSE)
+
+# save.image()
+proc.time() - ptm
